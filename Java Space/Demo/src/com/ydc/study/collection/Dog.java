bc6f518cccbcb89ce9381feb18042dd8b2e73538
Novan <15619966566@163.com>
2019-10-17 10:13:13 +0800

    增加代码规范。 增加忽略文件

--- Java Space/Demo/src/com/ydc/study/collection/Dog.java
+++ Java Space/Demo/src/com/ydc/study/collection/Dog.java
@@ -1,26 +1,45 @@
 package com.ydc.study.collection;
 
-public  class Dog {
-    public Dog(){}
-    public Dog(int biaohao, String name,String pingzhong, char sex, int qingmidu , int jiankangzhi)
-    {
-        setBiaohao(biaohao);
-        setName(name);
-        setPingzhong(pingzhong);
-        setSex(sex);
-        setQingmidu(qingmidu);
-        setJiankangzhi(jiankangzhi);
+
+/**
+ * @author Administrator
+ * <p>
+ * data 2019-10-17 10:10:51
+ */
+public class Dog {
+
+    public Dog() {
+
     }
-    public void jk()
-    {
-        if (getJiankangzhi()<=59)
-        {
+
+    /**
+     * @param number      编号
+     * @param name        名称
+     * @param variety     品种
+     * @param sex         性别
+     * @param intimacy    亲密关系
+     * @param healthLevel 健康程度
+     */
+    public Dog(int number, String name, String variety, char sex, int intimacy, int healthLevel) {
+        setBiaohao(number);
+        setName(name);
+        setPingzhong(variety);
+        setSex(sex);
+        setQingmidu(intimacy);
+        setJiankangzhi(healthLevel);
+    }
+
+    // TODO: 2019/10/17   以下代码按照按照上述修改。
+
+    public void jk() {
+        if (getJiankangzhi() <= 59) {
             System.out.println("狗狗的健康值小于60！ 领养失败");
         }
     }
+
     private int biaohao = 1;
     private String name = "豆豆";
-   private String pingzhong = "拉布拉多";
+    private String pingzhong = "拉布拉多";
     private char sex = '公';
     private int qingmidu = 50;
     private int jiankangzhi = 60;
@@ -32,7 +51,6 @@ public  class Dog {
     public void setPingzhong(String pingzhong) {
         this.pingzhong = pingzhong;
     }
-
 
 
     public int getBiaohao() {
